59ef3810d959d3a7baa1f671bf59a4dca03c3aa1
thehanielcode <haniel@etapath.com>
2025-02-25 10:05:15 +0200

    push

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
-Amplify.configure(amplifyconfig);
+
+const amplifyConfig = {
+  aws_project_region: import.meta.env.VITE_AWS_PROJECT_REGION,
+  aws_appsync_graphqlEndpoint: import.meta.env.VITE_AWS_APPSYNC_GRAPHQL_ENDPOINT,
+  aws_appsync_region: import.meta.env.VITE_AWS_APPSYNC_REGION,
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: import.meta.env.VITE_AWS_APPSYNC_API_KEY,
+  aws_cognito_identity_pool_id: import.meta.env.VITE_AWS_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_region: import.meta.env.VITE_AWS_COGNITO_REGION,
+  aws_user_pools_id: import.meta.env.VITE_AWS_USER_POOLS_ID,
+  aws_user_pools_web_client_id: import.meta.env.VITE_AWS_USER_POOLS_WEB_CLIENT_ID,
+  oauth: {},
+  aws_cognito_username_attributes: ["EMAIL", "PHONE_NUMBER"],
+  aws_cognito_social_providers: [],
+  aws_cognito_signup_attributes: ["EMAIL"],
+  aws_cognito_mfa_configuration: "OFF",
+  aws_cognito_mfa_types: ["SMS"],
+  aws_cognito_password_protection_settings: {
+    passwordPolicyMinLength: 8,
+    passwordPolicyCharacters: [],
+  },
+  aws_cognito_verification_mechanisms: ["EMAIL"],
+  aws_user_files_s3_bucket: import.meta.env.VITE_AWS_USER_FILES_S3_BUCKET,
+  aws_user_files_s3_bucket_region: import.meta.env.VITE_AWS_USER_FILES_S3_BUCKET_REGION,
+};
+
+Amplify.configure(amplifyConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
